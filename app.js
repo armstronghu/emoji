37e4bfc740e56a5e93834c9fa55643e4adccab18
huzhanchi <hzc1211@126.com>
2018-02-10 17:24:41 +0800

    1.0.3

--- app.js
+++ app.js
@@ -34,7 +34,6 @@ App({
     })
   },
   globalData: {
-    userInfo: null,
-    all:{}
+    userInfo: null
   }
 })
--- pages/index/index.js
+++ pages/index/index.js
@@ -6,23 +6,81 @@ Page({
     activeIndex: 0,
     emojis: emojis,   //只作为view渲染命令接口用，即单向到view
     emojiValues: "",
-    tipsShow: true
-  },
+    tipsShow: true,
+    copyAreaCursor: 0,
+    autoHeight: false,
+    height: 57,
 
+    indicatorDots:false,
+    autoplay: false,
+    swiperH:0,
+    currSwiper:0
+  },
+  order: ['people','food','nature','travel','objects','activity','symbols','flags'],
+  heights: [],
+  lastRow: 0,
+  state: 0,   //0:没有聚焦过  1:获得焦点 2:焦点失去事件处理完成
   viewData:{
-    emojiValues: ''
+    emojiValues: '',
+    copyAreaCursor: 0
   },
-  taginput: function(e){
-    var b = e.currentTarget.dataset.base
-    var v = e.detail.value
-    console.log(b)
-    all[b].tags = v
+  onLoad: function(){
+    var that = this
+    wx.getSystemInfo({
+      success: function (res) {
+        var sysW = res.screenWidth
+        that.heights[0] = Math.ceil(emojis.people.length * 40 / (sysW - 54)) + 1
+        that.heights[1] = Math.ceil(emojis.food.length * 40 / (sysW - 54)) +1
+        that.heights[2] = Math.ceil(emojis.nature.length * 40 / (sysW - 54)) +1
+        that.heights[3] = Math.ceil(emojis.travel.length * 40 / (sysW - 54)) +1
+        that.heights[4] = Math.ceil(emojis.objects.length * 40 / (sysW - 54)) +1
+        that.heights[5] = Math.ceil(emojis.activity.length * 40 / (sysW - 54)) +1
+        that.heights[6] = Math.ceil(emojis.symbols.length * 40 / (sysW - 54)) +1
+        that.heights[7] = Math.ceil(emojis.flags.length * 40 / (sysW - 54)) +1
+        that.setData({ swiperH: that.heights[0] * 40 })
+      }
+    });
   },
-  //emojis change event handle
+  swiperChange:function(e){
+    var cidx = e.detail.current
+    this.setData({ activeIndex: cidx, swiperH: this.heights[cidx] * 40})
+  },
+  onlinechange: function(e){
+    var flag = '+'
+    if (e.detail.lineCount > this.lastRow) flag = '+'
+    if (e.detail.lineCount < this.lastRow) flag = '-'
+    if (flag == '+' && e.detail.lineCount == 2 && this.data.autoHeight == false) {
+      this.setData({ autoHeight: true})
+    }
+    if (flag == '-' && e.detail.lineCount == 2 && this.data.autoHeight == true) {
+      this.setData({ autoHeight: false, height: 57})
+    }
+    this.lastRow = e.detail.lineCount
+  },
+  copyAreablur: function(e){
+    this.viewData.copyAreaCursor = e.detail.cursor
+    console.log(e.detail.cursor)
+    this.state = 2
+  },
+  copyAreafocus: function (e) {
+    this.state = 1
+  },
   tabEmoji: function (e) {
-    var p = e.currentTarget.dataset.point
-    this.viewData.emojiValues = this.viewData.emojiValues + convert(p)
-    this._emojiChanged(this.viewData.emojiValues)
+    var that = this
+    var event = e
+    var interval = setInterval(function(){
+      if(that.state == 0 || that.state == 2){
+        const add = convert(event.currentTarget.dataset.point)
+        const header = that.viewData.emojiValues.slice(0, that.viewData.copyAreaCursor)
+        const tail = that.viewData.emojiValues.slice(that.viewData.copyAreaCursor, that.viewData.emojiValues.length)
+        that.viewData.emojiValues = header + add + tail
+        that.viewData.copyAreaCursor = that.viewData.copyAreaCursor + add.length
+        that.setData({ copyAreaCursor: that.viewData.copyAreaCursor })
+        that._emojiChanged(that.viewData.emojiValues)
+        that.state = 0
+        clearInterval(interval)
+      }
+    },50)
   },
   copyAreaTypeing: function (e) {
     this.viewData.emojiValues = e.detail.value
@@ -31,7 +89,7 @@ Page({
 
   _emojiChanged: function (newEmojis) {
     this.setData({
-      emojiValues: newEmojis
+      emojiValues: newEmojis,
     })
     this._copy(newEmojis)
   },
@@ -54,20 +112,22 @@ Page({
     });
   },
   clearCopyArea: function () {
-    console.log('clear all')
     this._reset()
   },
   _reset: function () {
     this.viewData.emojiValues = ''
     this.setData({
-      emojiValues: ''
+      emojiValues: '',
+      autoHeight: false,
+      height: 57
     })
   },
  //tab click event handle
   tabClick: function (e) {
+    var nidx = e.currentTarget.id
     this.setData({
-      sliderOffset: e.currentTarget.offsetLeft,
-      activeIndex: e.currentTarget.id 
+      activeIndex: nidx,
+      currSwiper: nidx
     });
   },
   onPullDownRefresh: function(){
